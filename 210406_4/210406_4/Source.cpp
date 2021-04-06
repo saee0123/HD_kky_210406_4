4e6edc721263d03125fd70f2b925a730122733f7
saee0123 <saee0123@knou.ac.kr>
2021-04-06 23:23:29 +0900

    연습문제 4

---
+++ 210406_4/210406_4/Source.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void main() {
+	int _i5 = 5;
+	int _1(1), _2(2), _3(3), _4(4), _5(5), _6(6), _7(7), _8(8), _9(9);
+
+	printf ("구구단 5단을 출력하는 프로그램\n");
+
+	printf("5*1=%d \n", _i5 * _1);
+	printf("5*2=%d \n", _i5 * _2);
+	printf("5*3=%d \n", _i5 * _3);
+	printf("5*4=%d \n", _i5 * _4);
+	printf("5*5=%d \n", _i5 * _5);
+	printf("5*6=%d \n", _i5 * _6);
+	printf("5*7=%d \n", _i5 * _7);
+	printf("5*8=%d \n", _i5 * _8);
+	printf("5*9=%d", _i5 * _9);
+
+	fgetc(stdin);
+}
