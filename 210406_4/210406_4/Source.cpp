1ae3f45c4e4b1e6cd6dba8c0d4ad131645e14a11
saee0123 <saee0123@knou.ac.kr>
2021-04-06 23:26:40 +0900

    수정

--- 210406_4/210406_4/Source.cpp
+++ 210406_4/210406_4/Source.cpp
@@ -1,20 +1,19 @@
 #include<stdio.h>
 
 void main() {
-	int _i5 = 5;
-	int _1(1), _2(2), _3(3), _4(4), _5(5), _6(6), _7(7), _8(8), _9(9);
+	int _5 = 5;
 
 	printf ("구구단 5단을 출력하는 프로그램\n");
 
-	printf("5*1=%d \n", _i5 * _1);
-	printf("5*2=%d \n", _i5 * _2);
-	printf("5*3=%d \n", _i5 * _3);
-	printf("5*4=%d \n", _i5 * _4);
-	printf("5*5=%d \n", _i5 * _5);
-	printf("5*6=%d \n", _i5 * _6);
-	printf("5*7=%d \n", _i5 * _7);
-	printf("5*8=%d \n", _i5 * _8);
-	printf("5*9=%d", _i5 * _9);
+	printf("5*1=%d \n", _5 * 1);
+	printf("5*2=%d \n", _5 * 2);
+	printf("5*3=%d \n", _5 * 3);
+	printf("5*4=%d \n", _5 * 4);
+	printf("5*5=%d \n", _5 * 5);
+	printf("5*6=%d \n", _5 * 6);
+	printf("5*7=%d \n", _5 * 7);
+	printf("5*8=%d \n", _5 * 8);
+	printf("5*9=%d", _5 * 9);
 
 	fgetc(stdin);
 }
